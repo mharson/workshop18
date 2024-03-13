0f67896e8355f1ea207e005647ead4994926b587
Megan L Harson <150634753+mharson@users.noreply.github.com>
2024-03-12 21:58:13 -0500

    Add files via upload

---
+++ workshop18.js
@@ -0,0 +1,48 @@
+/*
+Unit Tests:
+
+1. A function called "multiplication" that returns the product of the two input numbers.
+Expect parameters must be numerical values
+Expect only 2 parameters
+Expect result to be one number
+Expect 1st and 2nd number to be multiplied
+
+
+2 A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+Expect parameters to be an array
+Expect elements of array to be numbers
+Recieve two arrays (no more or less) or error message
+Combine repeating elements; do not repeat values
+Resulting array should be in ascending order
+If no values are odd, return "all values are even"
+
+Functional Tests:
+
+1. A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+Expect the cart to be unchanged when user logs into account
+Expect the user will not be sent to another page when logging in
+Prompt user to reenter information if incorrect
+The items displayed in court should give a sum with taxes in the cart
+If a user has a tax exempt account, the cart should not add taxes
+No items should be removed or deleted from cart while logging into account
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
